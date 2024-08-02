bfbe87a8e6c30038507a79b275452e8eb37d34b2
vickvey <vickvey0303@gmail.com>
2024-08-02 23:03:53 +0530

    added functionality of choice submission to database

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index')
+    path('', views.Index.as_view(), name='index'),
+    path('question/<slug>', views.Question.as_view(), name='question')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import (
     ListView,
+    DetailView,
+    FormView
 )
+from django.views.generic.detail import SingleObjectMixin
 
-from main import models
+from main import (models, forms)
 
 # Create your views here.
 
@@ -11,3 +15,24 @@ class Index(ListView):
     model=models.Question
     template_name='main/index.html'
     context_object_name='questions'
+
+class Question(SingleObjectMixin, FormView):
+    model=models.Question
+    template_name='main/question.html'
+    form_class=forms.AnswerForm
+
+    def form_valid(self, form):
+        obj=form.save(commit=False)
+        obj.question=self.get_object()
+        obj.user=self.request.user
+        obj.save()
+        return HttpResponseRedirect('/')
+    
+    def post(self, request, *args, **kwargs):
+        self.object=self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        self.object=self.get_object()
+        context=self.get_context_data(object=self.object)
+        return self.render_to_response(context)
